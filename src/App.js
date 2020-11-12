5013e040c3e00359c29edb208a717abb356bb195
ZAFER OZCAN <zaferozzcan@gmail.com>
2020-11-12 18:58:18 -0500

    can fetch a question form API

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from "react";
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      questions: [],
+      score: 0,
+    };
+
+    // bind
+    this.getQuestions = this.getQuestions.bind(this);
+  }
+
+  getQuestions(event) {
+    event.preventDefault();
+    fetch("http://jservice.io/api/random")
+      .then((response) => {
+        return response.json();
+      })
+      .then(
+        (json) =>
+          this.setState({
+            questions: json,
+          }),
+        (err) => console.log(err)
+      );
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <main id="game-board">
+          <div className="container">
+            <h1>Welcome to Jeopardy</h1>
+            <form onSubmit={this.getQuestions}>
+              <input type="submit" value="Get Question" />
+            </form>
+          </div>
+        </main>
+      </div>
+    );
+  }
 }
-
-export default App;
